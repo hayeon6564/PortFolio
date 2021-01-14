2851c9336b747b6b14116f6464797ec79454b172
hayeon6564 <hayeon0203@gmail.com>
2021-01-14 19:21:53 +0900

    0114

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import back from './backimg.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+          <img src={back} width="100%" height="100%"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
